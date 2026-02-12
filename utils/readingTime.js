d3bb812f0f4e0fd52a75abfb3746e20bad48ad40
Chidiogo <chidiogoezeh.me@gmail.com>
2026-02-12 22:37:19 +0100

    updated readingTime.js to standard average reading time algorithm

--- utils/readingTime.js
+++ utils/readingTime.js
@@ -1,6 +1,6 @@
 export const calculateReadingTime = (text) => {
-    const wordsPerMinute = 200;
-    const numberOfWords = text.split(/\s/g).length;
-    const minutes = numberOfWords / wordsPerMinute;
-    return Math.ceil(minutes);
+    const wordsPerMinute = 200; // Average reading speed
+    const wordCount = text.trim().split(/\s+/).length;
+    const readingTime = Math.ceil(wordCount / wordsPerMinute);
+    return readingTime === 0 ? 1 : readingTime; // Minimum 1 minute
 };
